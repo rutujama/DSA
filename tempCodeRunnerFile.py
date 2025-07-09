4bca8dff2e94ba71213d4fcaef4a2680ab61ee81
Rutuja Mane <manerd@rknec.edu>
2025-07-09 10:34:38 +0530

    deque change

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,3 +3,7 @@ class Deque:
         self.items=[]
     def is_empty(self):
         return len(self.items)==0
+    def insert_front(self,data):
+        self.items.insert(0,data)
+    def insert_rear(self,data):
+        self.items.append(data) 
