d706c15b9e25931d03b22a95ee37e82249e310ee
Rutuja Mane <manerd@rknec.edu>
2025-07-10 11:14:46 +0530

    dequeue dll1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -10,3 +10,11 @@ class deque:
         self.item_count=0 
     def is_empty(self):
         return self.item_count==0
+    def insert_front(self,data):
+        n=Node(None,data,self.front)
+        if self.is_empty():
+            self.rear=n 
+        else:
+            self.front.prev=n 
+        self.front=n 
+        self.item_count+=1   
