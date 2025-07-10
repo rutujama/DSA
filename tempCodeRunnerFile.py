3046ccdd98b917e6f2f863e010b418bcaf9c2b20
Rutuja Mane <manerd@rknec.edu>
2025-07-10 10:51:37 +0530

    queue update1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -5,3 +5,13 @@ class Queue():
         return len(self.items)==0 
     def enqueue(self,data):
         self.items.append(data)
+    def dequeue(self):
+        if not self.is_empty():
+            self.items.pop(0)
+        else:
+            raise IndexError("queue is empty")
+    def get_front(self):
+        if not self.is_empty():
+            return self.items[0] 
+        else:
+            raise IndexError("queue is empty")     
