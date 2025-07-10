4f5c5408c7202944268986ebdc36e3cd1460ed04
Rutuja Mane <manerd@rknec.edu>
2025-07-10 11:04:10 +0530

    queue sll update1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -9,3 +9,11 @@ class Queue:
         self.item_count=0
     def is_empty(self):
         return self.item_count==0
+    def enqueue(self,data=None):
+        n=Node(data)
+        if self.is_empty():
+            self.front=n 
+        else:
+            self.rear.next=n 
+        self.rear=n 
+        self.item_count+=1 
