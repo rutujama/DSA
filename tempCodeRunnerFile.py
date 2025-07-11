253b992ca5412a73ec3ab95f9a8bd6f4ed131725
Rutuja Mane <manerd@rknec.edu>
2025-07-11 08:27:24 +0530

    stack by extending list1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,3 +3,13 @@ class Stack(list):
         return len(self)==None
     def push(self,data):
         return self.append(data)
+    def pop(self):
+        if not self.is_empty():
+            return super().pop()
+        else:
+            raise IndexError("Stack is empty")
+    def peek(self):
+        if not self.is_empty():
+            return self[-1]
+        else:
+            raise IndexError("Stack is empty")  
