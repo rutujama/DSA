7c84a3e50fde2e534c9c0951ae2f778771f8a9b5
Rutuja Mane <manerd@rknec.edu>
2025-07-11 08:32:12 +0530

    stack list1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,3 +3,13 @@ class Stack():
         self.items=[]
     def is_empty(self):
         return len(self.items)==None 
+    def push(self,data):
+        if not self.is_empty():
+            return self.items.append(data)
+        else:
+            raise IndexError("Stack is empty")
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack is empty")
