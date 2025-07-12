fe7b5b3949685a0250286527f407ce91e0032542
Rutuja Mane <manerd@rknec.edu>
2025-07-12 09:07:20 +0530

    priority by linked list1

--- prioritylinkedlist.py
+++ prioritylinkedlist.py
@@ -8,3 +8,15 @@ class priorityQueue:
     def __init__(self):
         self.start = None
         self.item_count = 0
+    def push(self, data, priority):
+        n = Node(data, priority)
+        if not self.start or priority < self.start.priority:
+            n.next = self.start
+            self.start = n
+        else:
+            temp = self.start
+            while temp.next and temp.next.priority <= priority:
+                temp = temp.next
+            n.next = temp.next
+            temp.next = n
+        self.item_count += 1    
