f02b89ad393ff08ff7a6c64bda31961eef264b62
Rutuja Mane <manerd@rknec.edu>
2025-07-12 09:03:15 +0530

    priority list1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,10 @@
 class priorityQueue:
     def __init__(self):
         self.items=[]
+    def push(self,data,priority):
+        index=0
+        while index<len(self.items) and self.items[index][1]<=priority:
+            index+=1
+        return self.items.insert(index,(data,priority))
+    def is_empty(self):
+        return len(self.items)==0    
