47d246b0868833c0b2dde1cf8031975b4c51f0f7
Rutuja Mane <manerd@rknec.edu>
2025-07-12 08:58:36 +0530

    stack by sll1

--- singly_inherit.py
+++ singly_inherit.py
@@ -2,3 +2,8 @@ from Singly_Linked_List import *
 class Stack(SLL):
     def is_empty(self):
         return super().is_empty()
+    def push(self,data):
+        self.insert_at_first(data)
+    def pop(self):
+        if not self.is_empty():
+          self.delete_first()    
