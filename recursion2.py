59e05a7fcfe3d1a5d9789cde41a5e59acb6f759f
Rutuja Mane <manerd@rknec.edu>
2025-07-16 23:56:00 +0530

    recursion2.b

--- recursion2.py
+++ recursion2.py
@@ -4,3 +4,8 @@ def printN(n):
     s=n+printN(n-1)
     return s
 print("sum is:",printN(10))
+def oddS(n):
+    if n==1:
+        return 1
+    return 2*n-1+oddS(n-1)
+print("odd sum is:",oddS(10)) 
