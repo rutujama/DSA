145b523f9f5db34d7c3996b419d48ce14d994274
Rutuja Mane <manerd@rknec.edu>
2025-07-16 23:56:46 +0530

    recursion2.c

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -9,3 +9,8 @@ def oddS(n):
         return 1
     return 2*n-1+oddS(n-1)
 print("odd sum is:",oddS(10)) 
+def evenS(n):
+    if n==1:
+        return 1
+    return 2*n+evenS(n-1)
+print("even sum is:",evenS(10)) 
