573d4eac8fbb9326bae3cf210800f5eb7649e375
Rutuja Mane <manerd@rknec.edu>
2025-07-16 23:59:16 +0530

    recursion2.g

--- recursion2.py
+++ recursion2.py
@@ -8,12 +8,12 @@ def oddS(n):
     if n==1:
         return 1
     return 2*n-1+oddS(n-1)
-print("odd sum is:",oddS(10)) 
+print("odd sum is:",oddS(18)) 
 def evenS(n):
     if n==1:
         return 1
     return 2*n+evenS(n-1)
-print("even sum is:",evenS(10)) 
+print("even sum is:",evenS(16)) 
 def fact(n):
     if n==1:
         return 1
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -8,19 +8,19 @@ def oddS(n):
     if n==1:
         return 1
     return 2*n-1+oddS(n-1)
-print("odd sum is:",oddS(10)) 
+print("odd sum is:",oddS(18)) 
 def evenS(n):
     if n==1:
         return 1
     return 2*n+evenS(n-1)
-print("even sum is:",evenS(10)) 
+print("even sum is:",evenS(16)) 
 def fact(n):
     if n==1:
         return 1
     return n*fact(n-1)
-print("factorial is:",fact(10))
+print("factorial is:",fact(15))
 def sqrN(n):
     if n==1:
         return 1
     return (n*(n+1)*(2*n+1))/6
-print("sqr of n:",sqrN(5)) 
+print("sqr of n:",sqrN(8)) 
