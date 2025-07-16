a1c33d5b72dce4f9a8c66f7895fb3fda659623c7
Rutuja Mane <manerd@rknec.edu>
2025-07-16 23:54:05 +0530

    recursion1

--- recursion.py
+++ recursion.py
@@ -9,3 +9,13 @@ def reverseN(n):
         print(n)
         reverseN(n-1)
 reverseN(10)   
+def oddN(n):
+    if n>0:
+        oddN(n-1)
+        print(2*n-1)
+oddN(9)    
+def evenN(n):
+    if n>0:
+        evenN(n-1)
+        print(2*n)
+evenN(9) 
